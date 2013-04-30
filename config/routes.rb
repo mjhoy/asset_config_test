df82a8bd7e353e0fdca1ccc63809efa25ff964ee
Michael Hoy <michael.john.hoy@gmail.com>
2013-04-30 12:19:35 -0400

    bin/rails g controller test hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 AssetConfigTest::Application.routes.draw do
+  get "test/hello"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
